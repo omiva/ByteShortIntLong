b82240444c69acd16485c64f4651887cd6b8a8ba
Yuvaraj M <yuvm19is@cmrit.ac.in>
2023-02-22 19:05:01 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+            int MyVal=1500;
+
+            long MinVal=Long.MIN_VALUE;
+            long MaxVal=Long.MAX_VALUE;
+
+            System.out.println(MinVal);
+            System.out.println(MaxVal);
+
+            long b=2147483__648l;
+            System.out.println(b);
+
+            short SmolNum= (short) (b/2);
+            System.out.println(SmolNum);
+    }
+}
